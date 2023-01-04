ee489f46f192366f783b875fbe7141bcd6e0e8bc
alitiger1381 <alitigercr71381@gmail.com>
2023-01-04 12:05:24 +0330

    sign up added

--- main.py
+++ main.py
@@ -12,21 +12,26 @@ except:
     print('Error while connecting to MySQL!')
 
 
-# while True:
-#     print('please choose one option')
-#     print('1: sign up\n2: login\n3: exit')
-#     com = int(input())
-#     if com == 1:
-#         user = input('please enter your username:')
-#         password = input('please enter your password:')
-#         address = input('please enter your address:')
-#         phone = input('please enter your phone:')
-#         email = input('please enter your email:')
-#         bdate = input('please enter your brthdate:')
-
-
-    # elif com == 2:
-    #     pass
-    # elif com == 3:
-    #     break
+while True:
+    print('please choose one option')
+    print('1: sign up\n2: login\n3: exit')
+    com = int(input())
+    if com == 1:
+        try:
+            user = input('please enter your username:')
+            password = input('please enter your password:')
+            address = input('please enter your address:')
+            phone = input('please enter your phone:')
+            email = input('please enter your email:')
+            bdate = input('please enter your brthdate:')
+            sql = 'INSERT INTO user (password, name, address, phone, email, birthdate, role) VALUES (%s, %s, %s, %s, %s, %s, %s)'
+            val = (password, user, address, phone, email, bdate, 'user')
+            cursor.execute(sql, val)
+            mydb.commit()
+        except:
+            print('error')
+    elif com == 2:
+        pass
+    elif com == 3:
+        break
 
