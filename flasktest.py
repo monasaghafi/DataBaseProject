c9eaca7302c1c0327d641d929f9acbf449a2d3b7
alitiger1381 <alitigercr71381@gmail.com>
2023-01-07 19:17:44 +0330

    jsonify mysql data

--- flasktest.py
+++ flasktest.py
@@ -1,7 +1,5 @@
-import json
-
 import mysql.connector
-from flask import Flask
+from flask import Flask, jsonify
 
 try:
     mydb = mysql.connector.connect(host='localhost', user='root', password='alidev12345', database='mydb')
@@ -9,23 +7,25 @@ except:
     print('Error while connecting to MySQL!')
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
 @app.route('/users')
 def index():
     cursor = mydb.cursor()
     cursor.execute('select * from user')
+
+    #get data and convert to json
     records = cursor.fetchall()
-    data = []
+    fields = cursor.description
+    columns = []
+    for f in fields:
+        columns.append(f[0])
+    jsonData = []
     for r in records:
-        d = {}
-        d['id'] = r[0]
-        d['name'] = r[1]
-        d['password'] = r[2]
-        d['address'] = r[3]
-        d['phone'] = r[4]
-        d['email'] = r[5]
-        d['birthdate'] = str(r[6])
-        d['role'] = r[7]
-        data.append(d)
-    return json.dumps(data)    
+        data_dict = {}
+        for i in range(len(columns)):
+            data_dict[columns[i]] = r[i]
+        jsonData.append(data_dict)
+    return jsonify({'users': jsonData})
    
 app.run()
