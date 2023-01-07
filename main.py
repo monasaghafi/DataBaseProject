660ac66f2b12c935cde99478ed3806e5cb55116b
alitiger1381 <alitigercr71381@gmail.com>
2023-01-07 22:34:18 +0330

    3 worst comments added

--- main.py
+++ main.py
@@ -1,33 +1,66 @@
 import mysql.connector
+from flask import Flask, jsonify, request, session
 
 try:
     mydb = mysql.connector.connect(host='localhost', user='root', password='alidev12345', database='mydb')
-    if mydb.is_connected():
-        cursor = mydb.cursor()
+    cursor = mydb.cursor(dictionary=True)
 except:
     print('Error while connecting to MySQL!')
 
+app = Flask(__name__)
+app.secret_key = '4lid3v5ecr3t'
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-while True:
-    print('please choose one option')
-    print('1: sign up\n2: login\n3: exit')
-    com = int(input())
-    if com == 1:
-        try:
-            user = input('please enter your username:')
-            password = input('please enter your password:')
-            address = input('please enter your address:')
-            phone = input('please enter your phone:')
-            email = input('please enter your email:')
-            bdate = input('please enter your brthdate:')
-            sql = 'INSERT INTO user (password, name, address, phone, email, birthdate, role) VALUES (%s, %s, %s, %s, %s, %s, %s)'
-            val = (password, user, address, phone, email, bdate, 'user')
-            cursor.execute(sql, val)
-            mydb.commit()
-        except:
-            print('error')
-    elif com == 2:
-        pass
-    elif com == 3:
-        break
+@app.route('/users')
+def getUsers():
+    if 'logged' not in session.keys() or not session['logged']:
+        return jsonify({'message': 'You have to login first!'})
+    else:
+        cursor.execute('select name, email, address, phone, birthdate, role from user')
+        #get data and convert to json
+        records = cursor.fetchall()
+        return jsonify({'users': records})
 
+@app.route('/register', methods=['GET'])
+def register():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    address = request.args.get('address')
+    phone = request.args.get('phone')
+    email = request.args.get('email')
+    bdate = request.args.get('bdate')
+    role = 'user'
+    cursor.execute('SELECT * FROM user WHERE name = %s', (username,))
+    user = cursor.fetchone()
+    if user:
+        return jsonify({'message': 'This username is already exists!'})
+    cursor.execute('INSERT INTO user (password, name, address, phone, email, birthdate, role) VALUES (%s, %s, %s, %s, %s, %s, %s)', (password, username, address, phone, email, bdate, role))
+    mydb.commit()
+    return jsonify({'message': 'user registered successfully!'})
+
+@app.route('/login', methods=['GET'])
+def login():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    cursor.execute('SELECT * FROM user WHERE name = %s and password = %s', (username, password))
+    user = cursor.fetchone()
+    if user:
+        session['logged'] = True
+        session['userid'] = user['id']
+        session['username'] = user['name']
+        session['role'] = user['role']
+        print(session)
+        return jsonify({'message': 'Logged in successfully!'})
+    else:
+        return jsonify({'message': 'username or password is incorrect!'})
+    
+@app.route('/threeworstcomments', methods=['GET'])
+def threeworstcomments():
+    pname = request.args.get('pname')
+    cursor.execute('SELECT user.name, comment.text, comment.rate FROM comment, product, user where comment.Product_name = product.name and comment.Customer_user_id = user.id and product.name = %s order by comment.rate limit 3', (pname,))
+    comments = cursor.fetchall()
+    return jsonify({'comments': comments})
+
+
+
+app.run(debug=True)
